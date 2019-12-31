ec1678dd7a06337fe88ea67b327d4607633ca2f0
Byeong Gui Jeong <jsmile@naver.com>
2019-12-31 22:46:18 +0900

    configuration by java file

---
+++ src/main/java/guru/springframework/config/HelloConfig.java
@@ -0,0 +1,27 @@
+package guru.springframework.config;
+
+import guru.springframework.services.HelloWorldService;
+import guru.springframework.services.HelloWorldServiceEnglishImpl;
+import guru.springframework.services.HelloWorldServiceKoreanImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class HelloConfig
+{
+   @Bean
+   @Profile( "english" )
+   public HelloWorldService helloWorldServiceEnglish()
+   {
+      return new HelloWorldServiceEnglishImpl();
+   }
+
+   @Bean
+   @Profile( { "dufault", "korean" } )
+   public HelloWorldService helloWorldServiceKorean()
+   {
+      return new HelloWorldServiceKoreanImpl();
+   }
+
+}
