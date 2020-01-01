b679fd1fad8105e6aeada6aade9355b6c0708863
Byeong Gui Jeong <jsmile@naver.com>
2020-01-01 14:45:25 +0900

    add some comments

--- src/main/java/guru/springframework/config/HelloConfig.java
+++ src/main/java/guru/springframework/config/HelloConfig.java
@@ -7,6 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+/**
+ * @Configuration class 를 통해
+ *  XML 설정이나 @Component 없이
+ *  단순한 Java Class 를 Bean 으로 등록할 수 있다.
+ * */
 @Configuration
 public class HelloConfig
 {
@@ -18,7 +23,7 @@ public class HelloConfig
    }
 
    @Bean
-   @Profile( { "default", "korean" } )
+   @Profile( { "default", "korean" } ) // @Profile 에 default 를 선언하면 .properties 파일에 선언이 없을 경우 기본값
    public HelloWorldService helloWorldServiceKorean()
    {
       return new HelloWorldServiceKoreanImpl();
