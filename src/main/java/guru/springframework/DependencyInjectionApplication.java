59cee43fc224a8459fd39c49f3c0f1d6998d126a
Byeong Gui Jeong <jsmile@naver.com>
2020-01-01 15:11:39 +0900

    add commit about injection of @Component

--- src/main/java/guru/springframework/DependencyInjectionApplication.java
+++ src/main/java/guru/springframework/DependencyInjectionApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+/**
+ *  같은 package 또는 하위 레벨 package 에 Annotation 으로 설정된 Bean 들은
+ *  Injection 을 받을 수 있으나
+ *  동일 레벨의 서로 다른 package 에 있는 Bean 들은 Injection 불가
+ * */
 @SpringBootApplication
 public class DependencyInjectionApplication
 {
