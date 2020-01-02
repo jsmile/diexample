f55ac566f104497c96f259478d49ee1a10cf56cf
Byeong Gui Jeong <jsmile@naver.com>
2020-01-02 18:41:39 +0900

    advanced autowire with @Primary, method refernence, name reference:

--- src/main/java/guru/springframework/controllers/GreedingController.java
+++ src/main/java/guru/springframework/controllers/GreedingController.java
@@ -6,36 +6,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
+/**
+ *  @Bean 으로 설정된 Bean 을 참조하는 방법에는
+ *  Bean 의 Method 명을 이용한 참조와
+ *  Bean 의 name 을 이용한 참조 등이 있다.
+ * */
 @Controller
 public class GreedingController
 {
    HelloWorldService helloWorldService;
    HelloWorldService helloWorldServiceSpanish;
+   HelloWorldService helloWorldServiceChinese;
 
    @Autowired
-   public void setHelloWorldService( HelloWorldService helloWorldService )
+   public void setHelloWorldService( HelloWorldService _helloWorldService )
    {
-      this.helloWorldService = helloWorldService;
+      this.helloWorldService = _helloWorldService;
    }
 
+   // Bean 의 Method 명을 이용한 참조
    @Autowired
    @Qualifier( "helloWorldServiceSpanish" )
-   public void setHelloWorldServiceSpanish( HelloWorldService helloWorldServiceSpanish )
+   public void setHelloWorldServiceSpanish( HelloWorldService _helloWorldServiceSpanish )
    {
-      this.helloWorldServiceSpanish = helloWorldServiceSpanish;
+      this.helloWorldServiceSpanish = _helloWorldServiceSpanish;
    }
 
+   // Bean 의 name 선언을 이용한 참조
    @Autowired
    @Qualifier( "chinese" )
-   public void setHelloWorldServiceChinese( HelloWorldService helloWorldServiceChinese )
+   public void setHelloWorldServiceChinese( HelloWorldService _helloWorldServiceChinese )
    {
-      this.helloWorldServiceSpanish = helloWorldServiceSpanish;
+      this.helloWorldServiceChinese = _helloWorldServiceChinese;
    }
 
    public String sayHello()
    {
       String greeding = helloWorldService.getGreeding();
       System.out.println( helloWorldServiceSpanish.getGreeding() );
+      System.out.println( helloWorldServiceChinese.getGreeding() );
       return greeding;
    }
 
