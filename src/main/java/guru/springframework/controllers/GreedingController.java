1c99d9e65924fe4858340d72dc2aa402af63d1dd
Byeong Gui Jeong <jsmile@naver.com>
2020-01-03 16:07:46 +0900

    XML Configuration Base

--- src/main/java/guru/springframework/controllers/GreedingController.java
+++ src/main/java/guru/springframework/controllers/GreedingController.java
@@ -1,13 +1,21 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.HelloWorldService;
+import guru.springframework.services.HelloWorldServiceSpanishImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
+/**
+ *  @Bean 으로 설정된 Bean 을 참조하는 방법에는
+ *  Bean 의 Method 명을 이용한 참조와
+ *  Bean 의 name 을 이용한 참조 등이 있다.
+ * */
 @Controller
 public class GreedingController
 {
    HelloWorldService helloWorldService;
+   HelloWorldService helloWorldServiceSpanish;
 
    @Autowired
    public void setHelloWorldService( HelloWorldService helloWorldService )
@@ -15,9 +23,27 @@ public class GreedingController
       this.helloWorldService = helloWorldService;
    }
 
+   // Bean 의 Method 명을 이용한 참조
+   @Autowired
+   @Qualifier( "helloWorldServiceSpanish" )
+   public void setHelloWorldServiceSpanish( HelloWorldService helloWorldServiceSpanish )
+   {
+      this.helloWorldServiceSpanish = helloWorldServiceSpanish;
+   }
+
+   // Bean 의 name 선언을 이용한 참조
+   @Autowired
+   @Qualifier( "chinese" )
+   public void setHelloWorldServiceChinese( HelloWorldService helloWorldServiceChinese )
+   {
+      this.helloWorldServiceSpanish = helloWorldServiceSpanish;
+   }
+
    public String sayHello()
    {
-      return helloWorldService.getGreeding();
+      String greeding = helloWorldService.getGreeding();
+      System.out.println( helloWorldServiceSpanish.getGreeding() );
+      return greeding;
    }
 
 }
